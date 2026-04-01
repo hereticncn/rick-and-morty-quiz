bb75af28ee420c41e994428820231c7b4941a470
Angelina Maksimovich <heretic.incen@gmail.com>
2026-04-01 21:39:30 +0300

    RICKANDMORTY: откорректирована ссылка для base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/hereticncn/rick-and-morty-quiz',
+  base: '/rick-and-morty-quiz/',
   plugins: [vue()],
 })
